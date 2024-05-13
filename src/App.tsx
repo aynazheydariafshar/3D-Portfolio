bff8aadf1cd0d18f9e46a7d42b15082a34ff22d0
aynaz <aynaz@arianalabs.io>
2024-05-13 16:07:22 +0330

    FEAT : install packages

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <main>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element="Home" />
+          <Route path="/about" element="About" />
+          <Route path="/projects" element="Projects" />
+          <Route path="/contact" element="Contact" />
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
+}
+export default App;
