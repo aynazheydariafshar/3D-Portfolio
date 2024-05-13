bf3025fe173d8c480884dd2c6b3a0041ad1dc661
aynaz <aynaz@arianalabs.io>
2024-05-13 16:35:19 +0330

    FEAT : build pages & routes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,28 @@
+import { NavLink } from "react-router-dom";
+
+const navbarItems: string[] = ["about", "projects", "contact"];
+
 export default function Navbar() {
-  return <h1>dddddddddddd</h1>;
+  return (
+    <header className="flex justify-between items-center p-4">
+      <NavLink
+        className="w-10 h-10 items-center font-bold justify-center flex bg-slate-100 text-blue-500 shadow-lg rounded-lg"
+        to="/"
+      >
+        <p className="">AH</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        {navbarItems.map((item: string) => (
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "text-blue-500" : "text-black"
+            }
+            to={`/${item}`}
+          >
+            {item.charAt(0).toUpperCase() + item.slice(1)}
+          </NavLink>
+        ))}
+      </nav>
+    </header>
+  );
 }
